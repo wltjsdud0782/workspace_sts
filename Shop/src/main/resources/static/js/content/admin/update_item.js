ce0c8442811bd463229a47f4af1595f647be245f
wltjsdud0782 <wltjsdud0782@gmail.com>
2024-02-14 16:42:41 +0900

    정보 변경 중완

--- Shop/src/main/resources/static/js/content/admin/update_item.js
+++ Shop/src/main/resources/static/js/content/admin/update_item.js
@@ -23,6 +23,7 @@ function itemDetailInfo(itemCode){
     })
     //fetch 통신 후 실행 영역
     .then((data) => {//data -> controller에서 리턴되는 데이터!
+        console.log(data);
         updateItemDetail.innerHTML='';
         let str = '';
 
